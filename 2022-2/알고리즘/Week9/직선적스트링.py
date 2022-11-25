d2b440b5da78c297f0d678617b8a2e2e755ab836
minyeong <106754293+miinyeong@users.noreply.github.com>
2022-11-25 20:14:48 +0900

    add: 9,10주차 실습

---
+++ 2022-2/알고리즘/Week9/직선적스트링.py
@@ -0,0 +1,25 @@
+def bruteForce(p,t,k):
+    M = len(p)
+    N = len(t)
+    i = k
+    j = 0
+    while j < M and i < N:
+        if t[i] != p[j]:
+            i -= j
+            j = -1
+        i += 1
+        j += 1
+    if j == M: return i-M
+    else: return i
+
+text = 'ababacabcbababcacacbcaababca'
+pattern = 'ababca'
+M = len(pattern)
+N = len(text)
+K = 0
+while True:
+    pos = bruteForce(pattern, text, K)
+    K = pos + M
+    if K < N: print('패턴이 나타난 위치 : ', pos)
+    else: break
+print('스트링 탐색 종료')
