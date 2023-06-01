ec1806c53163e52f63ffe24940aded501cea57c4
minyeong <106754293+miinyeong@users.noreply.github.com>
2023-06-01 16:28:36 +0900

    add: Java(week12)

---
+++ 2023-1/Java/week12/InnerClassListener2.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class InnerClassListener2 extends JFrame {
+	public InnerClassListener2() {
+		setTitle("Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		btn.addActionListener(new MyActionListener2());
+		c.add(btn);
+		setSize(350, 150);
+		setVisible(true);
+	}
+	private class MyActionListener2 implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if(b.getText().equals("Action"))
+				b.setText("액션");
+			else
+				b.setText("Action");
+			// InnerClassListener의 멤버나 JFrame의 멤버를 호출할 수 있음
+			InnerClassListener2.this.setTitle(b.getText()); // 프레임 타이틀에 버튼 문자열을 출력한다.
+		}
+	}
+	public static void main(String [] args) {
+		new InnerClassListener2();
+	}
+} 
